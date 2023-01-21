89867561fed2e4c5efaeb04b2fa85a6e3218b4e4
Mike <ali27mf@gmail.com>
2023-01-21 16:07:31 +0300

    Added the MdToHtml() finished and entry() not finished

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown 
 from . import util
 
+def MdToHtml(title):
+    markdowner = Markdown()
+    entryContent = util.get_entry(title)
+
+    if entryContent is not None:
+        return markdowner.convert(entryContent)
+    else:
+        return None
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry():
+    return
