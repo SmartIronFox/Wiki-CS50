115b024f2968fe72c22d5e2aa4c28564f73f50f1
Mike <ali27mf@gmail.com>
2023-01-21 16:52:51 +0300

    Added search() finished

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,25 +2,28 @@ from django.shortcuts import render
 from markdown2 import Markdown 
 from . import util
 
+# A function used to convert MD to HTML
 def MdToHtml(title):
     markdowner = Markdown()
     entryContent = util.get_entry(title)
 
-    if entryContent is not None:
+    if entryContent != None:
         return markdowner.convert(entryContent)
     else:
         return None
 
+# A function for the index.html page
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# A function for the wiki/TITLE page
 def entry(request, title):
     
     entryContent = MdToHtml(title)
 
-    if entryContent is not None:
+    if entryContent != None:
         return render(request, "encyclopedia/entry.html", {
             "entryContent": entryContent
         })
@@ -29,3 +32,27 @@ def entry(request, title):
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
+
+# A function for searching in all pages
+def search(request):
+    if request.method == "POST":
+        entryName = request.POST["q"]
+        entryContent = MdToHtml(entryName)
+
+        if entryContent != None:
+            return render(request, "encyclopedia/entry.html", {
+                "entryContent": entryContent
+            })
+        
+        else:
+
+            entries = util.list_entries()
+            results = []
+
+            for entry in entries:
+                if entryName.lower() in entry.lower():
+                    results.append(entry)
+            
+            return render(request, "encyclopedia/search.html", {
+                "results": results
+            })
