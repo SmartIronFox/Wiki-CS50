b0b126c3baa94dfac79e489d3a00adebaec1fcd2
Mike <ali27mf@gmail.com>
2023-01-23 17:33:43 +0300

    Finished the site, after adding the EditEntry() and RandomEntry() to the website and adding edit.html and improving layout.html and entry.html

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("New/", views.NewPage, name="new")
+    path("New/", views.NewPage, name="new"), 
+    path("edit/<str:title>", views.EditPage, name="edit"),
+    path("random/", views.RandomEntry, name="random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,6 +5,7 @@ from markdown2 import Markdown
 from . import util
 from django import forms
 from django.utils.safestring import mark_safe
+import random
 
 # A function used to convert MD to HTML
 def MdToHtml(title):
@@ -35,7 +36,8 @@ def entry(request, title):
 
     if entryContent != None:
         return render(request, "encyclopedia/entry.html", {
-            "entryContent": entryContent
+            "entryContent": entryContent,
+            "title": title
         })
 
     else:
@@ -66,9 +68,9 @@ def search(request):
             })
 
 # A class for all the inputs required to make a new page
-class entryinputs(forms.Form):
+class entryinputs(forms.Form,):
     title = forms.CharField(label="Title\n" , widget=forms.TextInput(attrs={"style":"display:block;"}))
-    content = forms.CharField(label="Content\n" ,widget=forms.Textarea(attrs={"style":"width: 90%; height:50%; display:block"}))
+    content = forms.CharField(label="Content\n" ,widget=forms.Textarea(attrs={"style":"width: 90%; height:60%; display:block"}))
 
 # A function to add a new entry to the website 
 def NewPage(request):
@@ -92,3 +94,31 @@ def NewPage(request):
         return render(request, "encyclopedia/new.html", {
             "inputs": entryinputs()
         })
+
+# A fucntion to edit existing entries
+def EditPage(request, title):
+    content = util.get_entry(title)
+    if content != None:
+        
+        if request.method == "POST":
+            entryNewContent = request.POST["content"]
+            util.save_entry(title, entryNewContent)
+            return HttpResponseRedirect(reverse("entry", kwargs={"title": title}))
+
+        else:
+            return render(request, "encyclopedia/edit.html",{
+                "title": title,
+                "content": content
+            })
+        
+    else: 
+        return showError(request, f"{title} is an Entry that doesn't exist.")
+        
+# A function that takes you to a random entry
+def RandomEntry(request):
+    entries = util.list_entries()
+    title = random.choice(entries)
+    
+    return HttpResponseRedirect(reverse("entry", kwargs={"title": title}))
+
+
